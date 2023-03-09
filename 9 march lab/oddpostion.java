e9a79cf32f3d364c8156cb92adffe6582c2cd392
Deepak <121931668+Deepak-0007@users.noreply.github.com>
2023-03-09 21:56:30 +0530

    odd position element print from array

--- 9 march lab/oddpostion.java
+++ 9 march lab/oddpostion.java
@@ -1,21 +1,21 @@
-package lab;
-import java.util.Scanner;
-public class oddpostion {
-public static void main(String[] args) {
-Scanner sc=new Scanner(System.in);
+package lab;// package name
+import java.util.Scanner;// import class
+public class oddpostion { //main class
+public static void main(String[] args) { // main method
+Scanner sc=new Scanner(System.in);  //Scanner class
 System.out.println("Enter the number of element of array ");
-int num=sc.nextInt();
-int arr[]=new int [num];
- for(int i=0;i<num;i++) {
-	 arr[i]=sc.nextInt();
+int num=sc.nextInt();// for input
+int arr[]=new int [num]; // array declare
+ for(int i=0;i<num;i++) { // looping 
+	 arr[i]=sc.nextInt();// array input
  }
  System.out.println("odd element of the array is ");
- for(int i = 0 ; i < num ; i++)
+ for(int i = 0 ; i < num ; i++) //array initialize
  {
 	
-     if(arr[i] % 2 != 0)
+     if(arr[i] % 2 != 0) // logic 
      {
-         System.out.print(arr[i]+" ");
+         System.out.print(arr[i]+" ");// print odd position element
      }
  }
 

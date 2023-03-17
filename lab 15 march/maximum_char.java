4f38c92077e0a311b5836bd91354547d25dee83f
Deepak <121931668+Deepak-0007@users.noreply.github.com>
2023-03-17 10:22:43 +0530

    Add files via upload

---
+++ lab 15 march/maximum_char.java
@@ -0,0 +1,6 @@
+public class maximum_char {// main class 
+    public static void main(String[] args) { // main method 
+        String str="hello in the world of java"; // string 
+        System.out.println("maximum char in String is : " +str.length());// no of the char in string
+    }
+}
